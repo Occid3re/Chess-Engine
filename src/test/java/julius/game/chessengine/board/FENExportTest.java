66f574145c2c987234f2ef3e434b6762966b8b22
Occid3re <juliussze@gmail.com>
2025-09-16 14:08:17 +0200

    Include full state in exported FEN

---
+++ src/test/java/julius/game/chessengine/board/FENExportTest.java
@@ -0,0 +1,21 @@
+package julius.game.chessengine.board;
+
+import julius.game.chessengine.engine.Engine;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FENExportTest {
+
+    @Test
+    void exportFenReflectsCompleteGameState() {
+        Engine engine = new Engine();
+        String expectedFen = "r3k2r/pppb1ppp/2n1bn2/2Pp4/3P4/2N1PN2/PPPB1PPP/R3K2R w KQkq d6 7 12";
+
+        engine.importBoardFromFen(expectedFen);
+
+        FEN exportedFen = engine.translateBoardToFen();
+
+        assertEquals(expectedFen, exportedFen.getRenderBoard());
+    }
+}
