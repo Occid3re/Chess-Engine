0f107b6a0a18af0e9f022768981e121c3fab8c41
Julius <juliussze@gmail.com>
2025-10-01 14:29:53 +0200

    More autotuning

--- src/test/java/julius/game/chessengine/ai/BestMoveSearchTest.java
+++ src/test/java/julius/game/chessengine/ai/BestMoveSearchTest.java
@@ -5,7 +5,6 @@ import julius.game.chessengine.board.Move;
 import julius.game.chessengine.engine.Engine;
 import julius.game.chessengine.utils.Score;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
