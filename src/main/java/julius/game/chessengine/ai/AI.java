af31a225a893fb19ab4641bb3dd6d949a7b13d06
Julius <juliussze@gmail.com>
2025-10-16 11:09:49 +0200

    epsilon 3

--- src/main/java/julius/game/chessengine/ai/AI.java
+++ src/main/java/julius/game/chessengine/ai/AI.java
@@ -1815,6 +1815,7 @@ public class AI {
 
         long curHash = simulation.getBoardStateHash();
 
+
         while (true) {
             if (!seen.add(curHash)) break;
             if (simulation.getGameState().isTerminal()) break;
