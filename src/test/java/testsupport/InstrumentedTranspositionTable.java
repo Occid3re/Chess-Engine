0c788b895c129a924a5015ce7c95337ddd903c5d
Julius <juliussze@gmail.com>
2025-10-19 00:52:01 +0200

    alpha

--- src/test/java/testsupport/InstrumentedTranspositionTable.java
+++ src/test/java/testsupport/InstrumentedTranspositionTable.java
@@ -14,6 +14,7 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public final class InstrumentedTranspositionTable<V> implements TranspositionTable<V> {
 
+
     private static final Logger log = LogManager.getLogger(InstrumentedTranspositionTable.class);
 
     private final Map<Long, V> store = new ConcurrentHashMap<>();
